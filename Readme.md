4cde387d250fea4f966455dd6c10396f1ca677c0
lcy19 <1901210420@pku.edu.cn>
2020-03-20 15:45:47 +0800

    Communicating between activities

---
+++ Readme.md
@@ -0,0 +1,1 @@
+根据图书Android Apprentice (Second Edition) Beginning Android Development with Kotlin，进行样例代码的编写。
---
+++ app/src/main/java/com/example/listmaker/MainActivity.kt
@@ -0,0 +1,86 @@
+package com.example.listmaker
+
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity() : AppCompatActivity(),
+    ListSelectionRecyclerViewAdapter.ListSelectionRecyclerViewClickListener {
+    lateinit var listsRecyclerView: RecyclerView
+    val listDataManager:ListDataManager = ListDataManager(this)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            showCreateListDialog()
+        }
+
+        val lists = listDataManager.readLists()
+        listsRecyclerView = findViewById(R.id.lists_recyclerview)
+        listsRecyclerView.layoutManager = LinearLayoutManager(this)
+        listsRecyclerView.adapter = ListSelectionRecyclerViewAdapter(lists, this)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun showCreateListDialog(){
+        val dialogTitle = getString(R.string.name_of_list)
+        val positiveButtonTitle = getString(R.string.create_list)
+
+        val builder =  AlertDialog.Builder(this)
+        val listTitleEditText = EditText(this)
+        builder.setTitle(dialogTitle)
+        builder.setView(listTitleEditText)
+        builder.setPositiveButton(positiveButtonTitle){ dialog, _ ->
+            val list = TaskList(listTitleEditText.text.toString())
+            listDataManager.saveList(list)
+
+            val recyclerAdapter = listsRecyclerView.adapter as ListSelectionRecyclerViewAdapter
+            recyclerAdapter.addList(list)
+            dialog.dismiss()
+            showListDetail(list)
+        }
+        builder.create().show()
+    }
+
+    private fun showListDetail(list:TaskList){
+        val listDetailIntent = Intent(this, ListDetailActivity::class.java)
+        listDetailIntent.putExtra(INTENT_LIST_KEY,list)
+        startActivity(listDetailIntent)
+    }
+
+    companion object{
+        const val INTENT_LIST_KEY = "list"
+    }
+
+    override fun listItemClicked(list: TaskList) {
+        showListDetail(list)
+    }
+}
